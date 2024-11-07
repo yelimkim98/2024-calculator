adec100393973106c1c83abf3e048c752ef7896a
yelimkim98 <kiel0103@naver.com>
2024-11-07 14:29:16 +0900

    initial commit

---
+++ src/main/java/naver/kiel0103/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package naver.kiel0103.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}

796eb640a55d980c037c6604b4a17978b4ad6754
yelimkim98 <kiel0103@naver.com>
2024-11-12 13:47:09 +0900

    fix: acceptance-test.sh 수정

--- acceptance-test.sh
+++ acceptance-test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1&b=2) -eq 2
+
+# HTTP GET 요청을 보내고 결과를 변수에 저장
+result=$(curl -s http://localhost:8765/sum?a=1&b=2)
+
+# 결과가 3인지 검사
+if test "$result" -eq 3; then
+    echo "The result is 3."
+else
+    echo "The result is not 3. It is $result."
+fi
